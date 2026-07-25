8429528be49cc72d5ef485062b2d361b5d7f59df
jaravanmobilellc-cloud <jaravanmobilellc@gmail.com>
2026-07-24 18:24:52 -0700

    cloudflare fix

--- nemt-production-app/astro.config.mjs
+++ nemt-production-app/astro.config.mjs
@@ -1,10 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+  // Explicitly set the output folders to align with the routing expectations
+  outDir: './dist',
+  build: {
+    server: './dist/server',
+    client: './dist/client'
+  }
 });
